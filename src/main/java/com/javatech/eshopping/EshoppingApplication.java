34fe63c3c67a011da77c9a8f24d1486aae5aa2d9
Sai Ram Dikondawar <sairam.dikondawar@tagnos.com>
2022-07-25 05:29:18 +0530

    initial commit

---
+++ src/main/java/com/javatech/eshopping/EshoppingApplication.java
@@ -0,0 +1,13 @@
+package com.javatech.eshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshoppingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javatech/eshopping/EshoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatech.eshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
